d99d128b36c2e09b853b5cd412a6157b631b9939
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-04-24 22:51:43 +0900

    update: remove commented code

--- go/controller/post.go
+++ go/controller/post.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"iteatter/domain"
 	"iteatter/infra"
 	"strconv"
@@ -10,56 +9,26 @@ import (
 )
 
 func AddPost(c *gin.Context) {
-	fmt.Println(c.PostForm("title"))
-	fmt.Println(c.PostForm("body"))
 	infra.DbCreate(domain.Post{
 		Title: c.PostForm("title"),
 		Body:  c.PostForm("body"),
 	})
 	c.Redirect(301, "/")
-	// 	id := 1 // データベースでautoincrementする？
-	// 	title := c.PostForm("title")
-	// 	body := c.PostForm("body")
-	// 	// store data
-	// 	model.Post{
-	// 		Id:    id,
-	// 		title: title,
-	// 		Body:  body,
-	// 	}
-	//
 }
 
 func GetPosts(c *gin.Context) {
-	//  select * from posts
-	// posts := []string{"taro", "jiro", "ichiro"}
 	posts := infra.DbReadAll()
-	// posts := []model.Post{}
-	// user1 := model.Post{
-	// 	Id:       1,
-	// 	Title:    "this is title",
-	// 	Body:     "hey\nim john",
-	// 	UserFrom: 100,
-	// }
-	// posts = append(posts, user1)
-	// posts = append(posts, user1)
-	// posts = append(posts, user1)
-	c.HTML(200, "list.html", gin.H{
+	c.HTML(200, "list", gin.H{
 		"posts": posts,
 	})
 }
 
 func GetOnePost(c *gin.Context) {
-	// select * from posts where id==??
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
 	}
 	post := infra.DbReadOne(id)
-	// post := model.Post{
-	// 	Id:    id,
-	// 	Title: "this is title",
-	// 	Body:  "hey\nim john",
-	// }
-	c.HTML(200, "show.html", gin.H{
+	c.HTML(200, "show", gin.H{
 		"post": post,
 	})
 }
