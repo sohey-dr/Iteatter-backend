9cd212b60dac25df31a6766b60b195b121018669
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-04-24 18:31:58 +0900

    add: add domain

---
+++ go/domain/post.go
@@ -0,0 +1,12 @@
+package domain
+
+import (
+	"github.com/jinzhu/gorm"
+)
+
+type Post struct {
+	gorm.Model
+	Title    string `gorm:"size:255"`
+	Body     string `gorm:"size:2048"`
+	FromUser int
+}
