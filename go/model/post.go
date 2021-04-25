85f02d0b4ba768f1e80937c4c927009b24a15c85
mochi-MizLab <h20ms419@hirosaki-u.ac.jp>
2021-04-25 13:30:01 +0900

    update: add api access point `api/`. resolve  #26

--- go/model/post.go
+++ go/model/post.go
@@ -9,3 +9,8 @@ type Post struct {
 	Title string `gorm:"size:255;not null"`
 	Body  string `gorm:"size:2048;not null"`
 }
+
+type PostRequest struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
