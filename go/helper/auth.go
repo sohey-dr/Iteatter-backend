c9ec61554b7d575cfec6620369221ba7b04dc373
sohey-H <60923547+sohey-dr@users.noreply.github.com>
2021-04-25 11:15:39 +0900

    Add session struct

--- go/helper/auth.go
+++ go/helper/auth.go
@@ -1,6 +1,13 @@
 package helper
 
-import "golang.org/x/crypto/bcrypt"
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
 
 //ハッシュ化
 func PasswordHash(pw string) (string, error) {
@@ -14,4 +21,28 @@ func PasswordHash(pw string) (string, error) {
 //ハッシュとマッチするかの確認
 func PasswordValid(hash, pw string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(pw))
+}
+
+type Session struct {
+	UserId interface{}
+}
+
+var LoginInfo Session
+func SessionCheck() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+			session := sessions.Default(c)
+			LoginInfo.UserId = session.Get("UserId")
+
+			// セッションがない場合、ログインフォームをだす
+			if LoginInfo.UserId == nil {
+					log.Println("ログインしていません")
+					c.Redirect(http.StatusMovedPermanently, "/login")
+					c.Abort() // これがないと続けて処理されてしまう
+			} else {
+					c.Set("UserId", LoginInfo.UserId) // ユーザidをセット
+					c.Next()
+			}
+			log.Println("ログインチェック終わり")
+	}
 }
